ad1d3f2f804ab0776b886ef11242ff8a09c69fd1
Emiliya Aghayeva <ag.emiliya@gmail.com>
2023-07-25 22:59:06 -0400

    initial commit

---
+++ rain/rain.js
@@ -0,0 +1,19 @@
+const timer= 5;
+let amountTime = timer * 60;
+
+
+function calculateTime(){
+    let countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime % 60;
+    if(seconds < 10){
+        seconds = "0"+ seconds;
+    }
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+    if(amountTime < 0){
+        amountTime=0;
+        clearInterval(time);
+    }    
+}
+let time = setInterval(calculateTime, 1000);
---
+++ waves/waves.js
@@ -0,0 +1,30 @@
+const timer= 5;
+let amountTime = timer * 60;
+
+
+function calculateTime(){
+    let countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime % 60;
+    if(seconds < 10){
+        seconds = "0"+ seconds;
+    }
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+    if(amountTime < 0){
+        amountTime=0;
+        clearInterval(time);
+    }    
+}
+let time = setInterval(calculateTime, 1000);
+
+const myButton = document.querySelector("#myButton");
+myButton.addEventListener("click", () => {
+    document.querySelector("#player").play();
+    document.querySelector("#myVideo").play();
+});
+
+
+
+
+
